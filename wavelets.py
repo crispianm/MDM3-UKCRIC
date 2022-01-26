5954ef6a8d53464fbd1db74ccc7fdb0a151db3d6
crispianm <45373428+crispianm@users.noreply.github.com>
2022-01-26 16:03:13 +0000

    Update wavelets.py

--- wavelets.py
+++ wavelets.py
@@ -13,15 +13,22 @@ df = pd.read_csv(join("./MATLAB/LabelledMatrixTimeDomain.csv"), header=None)
 data = df.iloc[0:20,3:].to_numpy()
 
 
-# # Loop through and save pngs of 7.5 sec chunks
-i = 1
-for row in data:
-    freq, times, Sxx = signal.cwt(row, 64, scaling='spectrum')
-    plt.pcolormesh(times, freq, Sxx, shading='gouraud')
-    # plt.axis('off')
-    plt.ylabel('Frequency [Hz]')
-    plt.xlabel('Time [sec]')
-    plt.tight_layout()
-    plt.savefig("./pngs/Spectogram %d.png" % i, bbox_inches='tight',transparent=False)
-    # plt.show()
-    i += 1
+# # # Loop through and save pngs of 7.5 sec chunks
+# i = 1
+# for row in data:
+#     freq, times, Sxx = signal.cwt(row, 64)
+#     plt.pcolormesh(times, freq, Sxx, shading='gouraud')
+#     # plt.axis('off')
+#     plt.ylabel('Frequency [Hz]')
+#     plt.xlabel('Time [sec]')
+#     plt.tight_layout()
+#     plt.savefig("./pngs/Spectogram %d.png" % i, bbox_inches='tight',transparent=False)
+#     # plt.show()
+#     i += 1
+
+sig  = data[0]
+widths = np.arange(1, 31)
+cwtmatr = signal.cwt(sig, signal.ricker, widths)
+plt.imshow(cwtmatr, extent=[-1, 1, 1, 31], cmap='RdBu', aspect='auto',
+           vmax=abs(cwtmatr).max(), vmin=-abs(cwtmatr).max())
+plt.show()
