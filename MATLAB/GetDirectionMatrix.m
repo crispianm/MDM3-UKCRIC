a52a1b13c00c76f8061815517c7df1ab276985da
HPapworth23 <ga19594@bristol.ac.uk>
2022-02-01 12:16:59 +0000

    Create GetDirectionMatrix.m

---
+++ MATLAB/GetDirectionMatrix.m
@@ -0,0 +1,21 @@
+%% Extracting direction matrix
+A=ShiftedDataMatrix;
+A(:,1) = [];
+DirectionMatrix=zeros(1,size(A,2)); %Create dummy row, gets removed after.
+for i = 1:size(A,1)
+    R=A(i,:);
+    if A(i,1)==1
+        if A(i,2)==0
+           DirectionMatrix=[DirectionMatrix;R]; %#ok<AGROW> 
+        end
+    else
+        if A(i,1)==0
+           if A(i,2)==1
+           DirectionMatrix=[DirectionMatrix;R]; %#ok<AGROW>
+           end
+        end
+    end
+end
+DirectionMatrix(1,:) = [];
+DirectionMatrix(:,2) = [];
+writematrix(DirectionMatrix, 'DiretionMatrix.csv')
