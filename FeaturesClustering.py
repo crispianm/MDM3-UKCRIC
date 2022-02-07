b6d500b692b886d4f96e7013de34c3d49c3f9756
Dan-UOB <ei19423@bristol.ac.uk>
2022-02-07 03:05:14 +0000

    update code

--- FeaturesClustering.py
+++ FeaturesClustering.py
@@ -5,69 +5,86 @@ from sklearn.decomposition import PCA
 import pandas as pd
 import numpy as np
 from sklearn.decomposition import NMF
+import mpl_toolkits
 
-plt.ioff()
+# plt.ioff()
+# plt.ion()
 
-RawData = pd.read_csv('./MATLAB/ShiftedLabelsMatrixTimeDomain.csv', header=None)
+RawData = pd.read_csv('./MATLAB/ExtractedFeatures_labelled_DM.csv', header=None)
 
 # print(RawData.values[:, 0:3])
 
-Labels = np.array(RawData.iloc[:, 0:3])
-Mean = np.array(RawData.iloc[:, 3])
-STD = RawData.iloc[:, 4]
-Max = RawData.iloc[:, 5]
-Min = RawData.iloc[:, 6]
-Kurt = RawData.iloc[:, 7]
-Skew = RawData.iloc[:, 8]
-Energy = RawData.iloc[:, 9]
-IQR = RawData.iloc[:, 10]
-MedianAbsDev = RawData.iloc[:, 11]
-MeanFreq = RawData.iloc[:, 12]
-MaxFreq = RawData.iloc[:, 13]
-MinFreq = RawData.iloc[:, 14]
-MaxPeak = RawData.iloc[:, 15]
-SumOfPeak = RawData.iloc[:, 16]
+Labels_len = 1
+Labels = np.array(RawData.iloc[:, 0:Labels_len])
+Mean = np.array(RawData.iloc[:, Labels_len+1])
+STD = RawData.iloc[:, Labels_len+2]
+Max = RawData.iloc[:, Labels_len+3]
+Min = RawData.iloc[:, Labels_len+4]
+Kurt = RawData.iloc[:, Labels_len+5]
+Skew = RawData.iloc[:, Labels_len+6]
+Energy = RawData.iloc[:, Labels_len+7]
+IQR = RawData.iloc[:, Labels_len+8]
+MedianAbsDev = RawData.iloc[:, Labels_len+9]
+MeanFreq = RawData.iloc[:, Labels_len+10]
+MaxFreq = RawData.iloc[:, Labels_len+11]
+MinFreq = RawData.iloc[:, Labels_len+12]
+MaxPeak = RawData.iloc[:, Labels_len+13]
+SumOfPeak = RawData.iloc[:, Labels_len+14]
 
-def To2DimPCA(Features):
-    pca = PCA(n_components=2)
+def To2DimPCA(Features, dims=2):
+    pca = PCA(n_components=dims)
     return pca.fit_transform(Features)
 
 def To2DimNMF(Features):
     nmf = NMF(n_components=2)
     return nmf.fit_transform(Features)
 
-def k_means(label, Data, name=None):
+def k_means(label, Data, name=None, dims=2):
     train_X, test_X, train_y, test_y = train_test_split(Data, label, test_size=0.33, random_state=1)
     kmeans = KMeans(n_clusters=max(label)+1, random_state=0)
     kmeans.fit(train_X)
     labels = kmeans.labels_
     predict_labels = kmeans.predict(test_X)
     print(kmeans.score(test_X, test_y))
-    # plt.figure()
-    plt.subplot(131)
-    plt.title('fit')
-    plt.scatter(train_X[:, 0], train_X[:, 1], c=labels.astype(float), edgecolor="k")
-    plt.subplot(132)
-    plt.title('predict')
-    plt.scatter(test_X[:, 0], test_X[:, 1], c=predict_labels.astype(float), edgecolor="k")
-    plt.subplot(133)
-    plt.title('ground truth')
-    scatter = plt.scatter(Data[:, 0], Data[:, 1], c=label.astype(float), edgecolor="k")
-    plt.legend(*scatter.legend_elements())
-    plt.savefig('./ClusterPlot/'+name+'.png')
-    plt.close()
-    # plt.show()
+    if dims==2:
+        plt.figure()
+        plt.subplot(131)
+        plt.title('fit')
+        plt.scatter(train_X[:, 0], train_X[:, 1], c=labels.astype(float), edgecolor="k")
+        plt.subplot(132)
+        plt.title('predict')
+        plt.scatter(test_X[:, 0], test_X[:, 1], c=predict_labels.astype(float), edgecolor="k")
+        plt.subplot(133)
+        plt.title('ground truth')
+        scatter = plt.scatter(Data[:, 0], Data[:, 1], c=label.astype(float), edgecolor="k")
+        plt.legend(*scatter.legend_elements())
+        if name!=None:
+            plt.savefig('./ClusterPlot/'+name+'.png')
+        # plt.close()
+    plt.show()
 
 
-# PCA_axis = To2DimPCA(RawData.iloc[:, 3:])
-NMF_axis = To2DimNMF(RawData.iloc[:, 3:])
-plt.figure()
-plt.scatter(NMF_axis[:, 0], NMF_axis[:, 1], c=RawData.iloc[:, 0].astype(float))
+PCA_axis = To2DimPCA(RawData.iloc[:, 3:], 3)
+fig = plt.figure()
+ax = fig.add_subplot(1, 2, 1, projection='3d')
+mpl_toolkits.mplot3d.Axes3D.scatter(ax, PCA_axis[:, 0], PCA_axis[:, 1], PCA_axis[:, 2], c=RawData.iloc[:, 0].astype(float))
+kmeans = KMeans(n_clusters=2, random_state=0)
+kmeans.fit(PCA_axis)
+labels = kmeans.labels_
+ax1 = fig.add_subplot(1, 2, 2, projection='3d')
+scatter = mpl_toolkits.mplot3d.Axes3D.scatter(ax1, PCA_axis[:, 0], PCA_axis[:, 1], PCA_axis[:, 2], c=labels.astype(float))
+plt.legend(*scatter.legend_elements())
 plt.show()
+
+
+# NMF_axis = To2DimNMF(RawData.iloc[:, 3:])
+# plt.figure()
+# plt.scatter(NMF_axis[:, 0], NMF_axis[:, 1], c=RawData.iloc[:, 0].astype(float))
+# plt.show()
 # print(Mean.shape, STD.shape, np.concatenate((Mean, STD), axis=0).shape)
 # print(np.stack((Kurt, Skew)).reshape(2, -1).shape)
+
 # k_means(RawData.iloc[:, 0], PCA_axis)  # K means using PCA axis
-# print([RawData.iloc[:, 3], RawData.iloc[:,4]])
 
 # Features = [Mean, STD, Max, Min, Kurt, Skew, Energy, IQR, MedianAbsDev,
 #             MeanFreq, MaxFreq, MinFreq, MaxPeak, SumOfPeak]
@@ -77,5 +94,5 @@ plt.show()
 #             Var = pd.concat([RawData.iloc[:, i], RawData.iloc[:, j]], axis=1).to_numpy()
 #             k_means(RawData.iloc[:, 0], Var, str(i)+'_'+str(j))
 
-Var = pd.concat([Kurt, Skew], axis=1).to_numpy()
-k_means(RawData.iloc[:, 0], Var[:])
+# Var = pd.concat([Kurt, Skew], axis=1).to_numpy()
+# k_means(RawData.iloc[:, 0], Var[:])
--- FindDirection.py
+++ FindDirection.py
@@ -2,18 +2,14 @@ from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-RawData = pd.read_csv('./MATLAB/ShiftedLabelsMatrixTimeDomain.csv', header=None)
-RX = RawData.iloc[:, 3:]
-Ry = RawData.iloc[:, 0]
-FeaturesData = pd.read_csv('./MATLAB/ExtractedFeatures_labelled.csv', header=None)
-X = FeaturesData.iloc[:, 3:]
+# FeaturesData = pd.read_csv('./MATLAB/ExtractedFeatures_labelled_DM.csv', header=None)
+FeaturesData = pd.read_csv('./MATLAB/removed_mislabelled_euclidean.csv', header=None)
+X = FeaturesData.iloc[:, 1:]
 y = FeaturesData.iloc[:, 0]  # Only use the total number of cars as label
 train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.33, random_state=1)
 
-print(i for i in test_y[:, 1] if i == 0)
-
 def RanForest(Data):
-    clf = RandomForestClassifier(max_depth=8, random_state=0)
+    clf = RandomForestClassifier(max_depth=12, random_state=0)
     '''
     Default param - 0.78767
     Bootstrap=False - 0.77691
@@ -22,20 +18,23 @@ def RanForest(Data):
     (Using 2nd col only as label) max_depth=8 - 0.80137,  max_depth=7 - 0.80528
     (Using 3rd col only as label) max_depth=8 - 0.78865,  max_depth=9 - 0.79159
     max_depth=9 - 0.796477 (col 3:9)
+    --------Mislabelled Removed---------
+    max_depth=9 - 0.82312
+    
     '''
     clf.fit(Data[0], Data[2])
     # predict = clf.predict(test_X)
     Score = clf.score(Data[1], Data[3])
-    print(Score)
+    print('Score from Random Forest Classifier: %s' % Score)
 
 def AdaBoost(Data):
-    clf = AdaBoostClassifier(n_estimators=100, learning_rate=0.5)
+    clf = AdaBoostClassifier(n_estimators=500, learning_rate=0.5)
     # Default param - 0.37769
     clf.fit(Data[0], Data[2])
     # predict = clf.predict(test_X)
     Score = clf.score(Data[1], Data[3])
-    print(Score)
+    print('Score from AdaBoost Classifier: %s' % Score)
 
 
 RanForest([train_X, test_X, train_y, test_y])
-# AdaBoost([train_X, test_X, train_y, test_y])
+AdaBoost([train_X, test_X, train_y, test_y])
